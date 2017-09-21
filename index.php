cdbc55d7b84292e3cddc302c55ce401061980c60
Adam Christopher B Widjaja <adam.keristoper@gmail.com>
2017-09-21 11:04:51 +0900

    HTML validation fix

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
+<!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="Welcome to RSAD funeral home Bali. Our funeral home is based in Denpasar, Bali. We provide funeral services, emergency transportation, and various types of coffins.">
-		<meta name="keywords" content="funeral home, rsad, denpasar, bali">
-		<meta name="author" content="Adam Christopher Backlas Widjaja">
-		<title>Bali Funeral Home RSAD Denpasar</title>
-		<link rel="stylesheet" href="css/style.css">
-	</head>
-	<body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Welcome to RSAD funeral home Bali. Our funeral home is based in Denpasar, Bali. We provide funeral services, emergency transportation, and various types of coffins.">
+    <meta name="keywords" content="funeral home, rsad, denpasar, bali">
+    <meta name="author" content="Adam Christopher Backlas Widjaja">
+    <title>Bali Funeral Home RSAD Denpasar</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
 
-	<script>
+  <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -19,120 +20,120 @@
   ga('create', 'UA-93870406-1', 'auto');
   ga('send', 'pageview');
 
-	</script>
+  </script>
 
-		<!--TOP AREA-->
-		<div class="top">
-			<div class="top-screen"></div>
-			<div class="top-content">
-				<p class="top-logo center"><img src="img/logo.png" alt="Rumah Duka RSAD"></p>
-				<h1 class="topmessage">Welcome to RSAD funeral home Bali</h1>
-				<div class="top-info center">
-					<p class="top-info-addr">PB. Sudirman Rd. No.1<br>Denpasar - Bali</p>
-					<div class="top-info-pwrap">
-						<div class="top-info-p">
-							<p>Tel.</p>
-							<p><a href="tel:+628113990430">+62811-399-0430</a></p>
-						</div><!--
-				 --><div class="top-info-p">
-							<p>Mobile</p>
-							<p><a href="tel:+628123882386">+6281-2388-2386</a></p>
-						</div><!--
-				 --><div class="top-info-p"><p>Mail us</p><a href="mailto:funeralhomebali@gmail.com?subject=RSAD Funeral Home Mail Inquiry" id="mail" class="mail"><img src="img/mail_icon.png" alt="mail">&nbsp;Contact</a></div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!--/TOP AREA-->
+  <!--TOP AREA-->
+  <div class="top">
+    <div class="top-screen"></div>
+    <div class="top-content">
+      <p class="top-logo center"><img src="img/logo.png" alt="Rumah Duka RSAD"></p>
+      <h1 class="topmessage">Welcome to RSAD funeral home Bali</h1>
+      <div class="top-info center">
+        <p class="top-info-addr">PB. Sudirman Rd. No.1<br>Denpasar - Bali</p>
+        <div class="top-info-pwrap">
+          <div class="top-info-p">
+            <p>Tel.</p>
+            <p><a href="tel:+628113990430">+62811-399-0430</a></p>
+          </div><!--
+           --><div class="top-info-p">
+            <p>Mobile</p>
+            <p><a href="tel:+628123882386">+6281-2388-2386</a></p>
+          </div><!--
+          --><div class="top-info-p"><p>Mail us</p><a href="mailto:funeralhomebali@gmail.com?subject=RSAD%20Funeral%20Home%20Mail%20Inquiry" id="mail" class="mail"><img src="img/mail_icon.png" alt="mail">&nbsp;Contact</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/TOP AREA-->
 
-		<!--LAYANAN-->
-		<div class="lyn">
-			<h2 class="lyn-title">Services</h2>
-			<div class="lynwrap">
-				<div class="lyn-item">
-					<p class="lyn-img"><a><img src="img/lyn1.jpg" alt="Rumah Duka"></a></p>
-					<p class="lyn-desc"><a>Funeral Home</a></p>
-				</div><!--
-		 --><div class="lyn-item">
-					<p class="lyn-img"><a><img src="img/lyn2.jpg" alt="Ruangan"></a></p>
-					<p class="lyn-desc"><a>Rooms</a></p>
-				</div><!--
-		 --><div class="lyn-item">
-					<p class="lyn-img"><a><img src="img/lyn3.jpg" alt="Transportasi"></a></p>
-					<p class="lyn-desc"><a>Transportation</a></p>
-				</div>
-			</div>
-			<h2 class="lyn-title lyn-title--coffin">Coffins</h2>
-			<div class="section section--coffin-top">
-				<div class="coffin-item coffin-item--top">
-					<img src="img/peti1.png" alt="coffin1" class="coffin-item__img">
-				</div>
-			</div>
-			<div class="section">
-				<div class="coffin-item coffin-item--1">
-					<img src="img/peti2.png" alt="coffin2" class="coffin-item__img">
-				</div><!--
-		 --><div class="coffin-item coffin-item--2">
-					<img src="img/peti3.png" alt="coffin3" class="coffin-item__img">
-				</div>
-			</div>
-		</div>
-		<!--/LAYANAN-->
+  <!--LAYANAN-->
+  <div class="lyn">
+    <h2 class="lyn-title">Services</h2>
+    <div class="lynwrap">
+      <div class="lyn-item">
+        <p class="lyn-img"><a><img src="img/lyn1.jpg" alt="Rumah Duka"></a></p>
+        <p class="lyn-desc"><a>Funeral Home</a></p>
+      </div><!--
+       --><div class="lyn-item">
+        <p class="lyn-img"><a><img src="img/lyn2.jpg" alt="Ruangan"></a></p>
+        <p class="lyn-desc"><a>Rooms</a></p>
+      </div><!--
+       --><div class="lyn-item">
+        <p class="lyn-img"><a><img src="img/lyn3.jpg" alt="Transportasi"></a></p>
+        <p class="lyn-desc"><a>Transportation</a></p>
+      </div>
+    </div>
+    <h2 class="lyn-title lyn-title--coffin">Coffins</h2>
+    <div class="section section--coffin-top">
+      <div class="coffin-item coffin-item--top">
+        <img src="img/peti1.png" alt="coffin1" class="coffin-item__img">
+      </div>
+    </div>
+    <div class="section">
+      <div class="coffin-item coffin-item--1">
+        <img src="img/peti2.png" alt="coffin2" class="coffin-item__img">
+      </div><!--
+       --><div class="coffin-item coffin-item--2">
+       <img src="img/peti3.png" alt="coffin3" class="coffin-item__img">
+      </div>
+    </div>
+  </div>
+  <!--/LAYANAN-->
 
-		<!--LAINNYA-->
-		<div class="lnn">
-			<h2 class="lnn-title">Other Facilities</h2>
-			<ul class="lnn-ul">
-				<li class="lnn-item">
-					<p>Cremation &amp; Burial</p>
-				</li>
-				<li class="lnn-item">
-					<p>Embalming (Formalin)</p>
-				</li>
-				<li class="lnn-item">
-					<p>Delivery cargo (National &amp; International)</p>
-				</li>
-				<li class="lnn-item">
-					<p>Death Certificate</p>
-				</li>
-				<li class="lnn-item">
-					<p>Flower Arrangements</p>
-				</li>
-				<li class="lnn-item">
-					<p>Advertisements in Newspapers</p>
-				</li>
-				<li class="lnn-item">
-					<p>Omnibus, etc.</p>
-				</li>
-			</ul>
-		</div>
-		<!--/LAINNYA-->
+  <!--LAINNYA-->
+  <div class="lnn">
+    <h2 class="lnn-title">Other Facilities</h2>
+    <ul class="lnn-ul">
+      <li class="lnn-item">
+        <p>Cremation &amp; Burial</p>
+      </li>
+      <li class="lnn-item">
+        <p>Embalming (Formalin)</p>
+      </li>
+      <li class="lnn-item">
+        <p>Delivery cargo (National &amp; International)</p>
+      </li>
+      <li class="lnn-item">
+        <p>Death Certificate</p>
+      </li>
+      <li class="lnn-item">
+        <p>Flower Arrangements</p>
+      </li>
+      <li class="lnn-item">
+        <p>Advertisements in Newspapers</p>
+      </li>
+      <li class="lnn-item">
+        <p>Omnibus, etc.</p>
+      </li>
+    </ul>
+  </div>
+  <!--/LAINNYA-->
 
-		<!--AKSES-->
-		<div class="acs">
-			<h2 class="acs-title">Access</h2>
-			<div class="acs-content">
-				<div class="acs-left">
-					<div class="acs-mapwrap"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.2996389894815!2d115.21714211482009!3d-8.663026993775274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc77b5a02f283d610!2sRSAD+Funeral+Home!5e0!3m2!1sen!2sjp!4v1494849765453" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></div>
-					<p class="acs-mapbtn"><a href="https://www.google.co.jp/maps/place/RSAD+Funeral+Home/@-8.663027,115.2171421,17z/data=!4m8!1m2!2m1!1srsad+funeral+home+denpasar!3m4!1s0x0:0xc77b5a02f283d610!8m2!3d-8.6628773!4d115.2203648" target="_blank">Open in Google Maps</a></p>
-				</div><!--
-		 --><div class="acs-right">
-					<p class="acs-addr">PB. Sudirman Rd. No.1<br>Denpasar - Bali</p>
-					<div class="acs-tel">
-						<p>Tel.</p>
-						<p><a href="tel:+628113990430">+62811-399-0430</a></p>
-						<p class="acs-tel-mt">HP</p>
-						<p><a href="tel:+628123882386">+6281-2388-2386</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!--/AKSES-->
+  <!--AKSES-->
+  <div class="acs">
+    <h2 class="acs-title">Access</h2>
+    <div class="acs-content">
+      <div class="acs-left">
+        <div class="acs-mapwrap"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.2996389894815!2d115.21714211482009!3d-8.663026993775274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc77b5a02f283d610!2sRSAD+Funeral+Home!5e0!3m2!1sen!2sjp!4v1494849765453" width="600" height="450" style="border:0" allowfullscreen></iframe></div>
+        <p class="acs-mapbtn"><a href="https://www.google.co.jp/maps/place/RSAD+Funeral+Home/@-8.663027,115.2171421,17z/data=!4m8!1m2!2m1!1srsad+funeral+home+denpasar!3m4!1s0x0:0xc77b5a02f283d610!8m2!3d-8.6628773!4d115.2203648" target="_blank">Open in Google Maps</a></p>
+      </div><!--
+       --><div class="acs-right">
+       <p class="acs-addr">PB. Sudirman Rd. No.1<br>Denpasar - Bali</p>
+      <div class="acs-tel">
+        <p>Tel.</p>
+        <p><a href="tel:+628113990430">+62811-399-0430</a></p>
+        <p class="acs-tel-mt">HP</p>
+        <p><a href="tel:+628123882386">+6281-2388-2386</a></p>
+      </div>
+      </div>
+    </div>
+  </div>
+  <!--/AKSES-->
 
-		<!--FOOTER-->
-		<footer class="ftr">
-			<p class="ftr-img"><img src="img/logofoot.png" alt="Footer Logo"></p>
-		</footer>
-		<!--/FOOTER-->
-	</body>
+  <!--FOOTER-->
+  <footer class="ftr">
+    <p class="ftr-img"><img src="img/logofoot.png" alt="Footer Logo"></p>
+  </footer>
+  <!--/FOOTER-->
+  </body>
 </html>
